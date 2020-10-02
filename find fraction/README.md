573809a854e7e785a36406390c75b52d53cf9214
paolo <p3.schiaovni@gmail.com>
2020-10-02 22:39:50 +0200

    Changes

--- find fraction/README.md
+++ find fraction/README.md
@@ -6,3 +6,7 @@ but the fraction found isn't about the right number written by the user
 the programs works with the fractions knowing that them are rounded
 for example, if i write 1.142857143, the programs doesn't find 
 1142857143/1000000000, but it finds 8/7.
+
+in the program find_fr_to_import.py there may be some modifies that there aren't
+in find_fraction.py, because sometimes the one to import gets some uploads to get
+optimized with all the other programs.
--- find fraction/find_fraction.py
+++ find fraction/find_fraction.py
@@ -1,19 +1,22 @@
 import time
 
 num=input('num: ')
-num=round(float(num), 8)
 
 def find_fraction(num, start):
+    num=round(float(num), 8)
+    i=0
 
-	for i in range(1, 10001):
-	    for j in range(1, 10001):
-	        if str(round((i/j), 8))==str(float(num)):
-	            print(str(i)+'/'+str(j))
-	            finish=time.perf_counter()
-	            print(f'program finished in {finish-start} seconds')
-	            exit()
+    while True:
+        i+=1
+        for j in range(abs(round(i/num)-1), round(i/num)+1):
+            if j!=0:
+                if str(round((i/j), 8))==str(float(num)):
+                    print(str(i)+'/'+str(j))
+                    finish=time.perf_counter()
+                    print(f'program finished in {finish-start} seconds')
+                    exit()
 
 start=time.perf_counter()
 find_fraction(num, start)
 finish=time.perf_counter()
-print(finish-start)
+print(f'nessuna frazione trovata, \nprogramma finito in {finish-start} secondi')
