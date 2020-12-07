9862f72880e89e0ff35754db253efdd411181cb2
paolo <p3.schiaovni@gmail.com>
2020-12-07 10:04:24 +0100

    selenium projects

---
+++ selenium/caso.py
@@ -0,0 +1,4 @@
+a=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print('ciao')
+print(len(a)/2)
+print(int(list[len(a)/2])
---
+++ selenium/priceproducts.py
@@ -0,0 +1,72 @@
+'''
+un programma che analizza i prezzi di un determinato prodotto su amazon
+Schiavoni Paolo, 07/12/2020
+'''
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+PATH='C:\Program Files (x86)\chromedriver.exe'
+driver=webdriver.Chrome(PATH)
+
+driver.get('https://www.amazon.it/')
+
+driver.implicitly_wait(3)
+
+searchbox=driver.find_element_by_xpath('//*[@id="twotabsearchtextbox"]')
+
+nomeprodotto=str(input('nome del prodotto: '))
+
+searchbox.send_keys(nomeprodotto)
+searchbox.send_keys(Keys.ENTER)
+
+lista=[]
+
+for el in driver.find_elements_by_class_name('a-price-whole'):
+    el=el.text.replace('.', '')
+    el=el.replace(' ', '')
+    if el!='':
+        el=float(el.replace(',', '.'))
+        if el>1:
+            lista.append(el)
+lista.sort()
+
+
+max=0
+min=10000000000
+
+media=0
+for el in lista:
+    if el>max:
+        max=el
+    if el<min:
+        min=el
+    media+=el
+media/=len(lista)
+
+mediana=0
+if len(lista)%2 !=0:#elementi dispari: trovo quello centrale
+    mediana=lista[int((len(lista)-1)/2)]
+elif len(lista)%2==0:
+    a=lista[int(len(lista)/2)]
+    b=lista[int((len(lista)-2)/2)]
+    mediana=(a+b)/2
+
+moda=0
+frequenza=0
+for i in lista:
+    frequenzatmp=-1
+    for j in lista:
+        if j-0.7<i and j+0.7>i:
+            frequenzatmp+=1
+    if frequenzatmp>frequenza:
+        moda=i
+        frequenza=frequenzatmp
+
+
+print('\nnumero di prodotti:', len(lista), '\npiù costoso:', \
+max, '\npiù economico:', min, '\nmedia dei prezzi:', media, \
+'\nmediana dei prezzi:', mediana, '\nmoda', moda, '('+str(frequenza)+')')
